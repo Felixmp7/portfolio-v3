f96dd183a12c5264af8283316934903e2aff3231
Felix Pacheco <devfex.19@gmail.com>
2023-08-12 12:39:50 -0400

    Implement SocialMedia

---
+++ src/app/components/SocialMedia.tsx
@@ -0,0 +1,21 @@
+'use client';
+import Link from 'next/link';
+import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5';
+
+export const SocialMedia = () => {
+    return (
+        <ul className='inline-flex items-center text-2xl gap-x-6'>
+            <li className='hover:text-blue-500'>
+                <Link href="https://www.linkedin.com/in/felix-pacheco-30/" target='_blank'>
+                    <IoLogoLinkedin />
+                </Link>
+            </li>
+            <li className='hover:text-white'>
+                <Link href="https://github.com/Felixmp7" target='_blank'>
+                    <IoLogoGithub />
+                </Link>
+            </li>
+        </ul>
+
+    );
+};
