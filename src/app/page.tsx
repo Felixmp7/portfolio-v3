82b842b170c1a1b69a2330a9360eaa51d6ef7657
Felix Pacheco <devfex.19@gmail.com>
2023-08-12 12:39:22 -0400

    Export home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,24 @@
-'use client';
-
-import Image from 'next/image';
-import clsx from 'clsx';
-import { useEffect, useState } from 'react';
+import { LatestWork } from '@/components/home/latest-work/LatestWork';
+import { MainHeader } from '@/components/home/MainHeader';
+import { MainRowImages } from '@/components/home/MainRowImages';
+import { MainStack } from '@/components/home/MainStack';
 
 
 export default function Home() {
-    const [mounted, setMounted] = useState(false);
-
-    const avatarHeader = clsx({
-        'rounded-full': true,
-        'relative duration-300 ease-out translate-y-20': mounted
-    });
-
-    useEffect(() => {
-        setMounted(true);
-    }, []);
-
     return (
-        <main>
-            <header className='container relative mx-auto'>
-                <Image
-                    priority
-                    src="/profile.jpeg"
-                    alt="Felix Pacheco Avatar"
-                    className={avatarHeader}
-                    width={80}
-                    height={80}
-                />
-            </header>
-        </main>
+        <section className="pt-12">
+            <MainHeader />
+            <section className='grid grid-cols-2 gap-10 mt-12 md:mt-24 md:grid-cols-4 h-80 xl:scale-105 md:h-60 lg:h-80'>
+                <MainRowImages />
+            </section>
+            <div className='w-full h-[2px] my-8 md:my-20 bg-zinc-300 dark:bg-zinc-700' />
+            <section>
+                <h2 className='text-3xl font-medium select-none md:text-5xl'>Main <strong className='font-extrabold hover:underline'>Stack</strong></h2>
+                <div className='mt-10 md:grid md:grid-cols-2 md:gap-16'>
+                    <MainStack />
+                    <LatestWork />
+                </div>
+            </section>
+        </section>
     );
 }
