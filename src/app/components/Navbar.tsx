49f6a5baebcfed16143d881a385521b41f8a68d4
Felix Pacheco <devfex.19@gmail.com>
2023-08-12 12:39:38 -0400

    Implement Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,22 +1,30 @@
 'use client';
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
-import { IoSunnyOutline } from 'react-icons/io5';
+import { useTheme } from "next-themes";
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { IoMoon, IoSunnyOutline } from 'react-icons/io5';
+import { NavbarItems } from './NavbarItems';
 
 export const Navbar = () => {
     const pathname = usePathname();
+    const { theme, setTheme } = useTheme();
+    const isDarkTheme = theme === "dark";
+
     const isHomePath = pathname === '/';
     const navClasses = clsx({
-        'flex items-center px-8 mt-10 text-sm font-normal text-gray-300' : true,
+        'flex items-center text-sm font-normal' : true,
         'gap-5 justify-end' : isHomePath,
         'justify-between' : !isHomePath
     });
     const avatarTranslatedClasses = clsx({'hidden': isHomePath});
+
+    const handleToggleTheme = () => isDarkTheme ? setTheme('light'): setTheme("dark");
+
     return (
-        <header className='container relative mx-auto'>
+        <header className='pt-10'>
             <nav className={navClasses}>
                 <Link href="/" className={avatarTranslatedClasses}>
                     <Image
@@ -28,22 +36,11 @@ export const Navbar = () => {
                         height={40}
                     />
                 </Link>
-                <ul className="flex gap-8 px-8 py-3 bg-zinc-800 rounded-3xl">
-                    <li className="duration-150 ease-in hover:text-pink-600">
-                        <Link href="/about">About</Link>
-                    </li>
-                    <li className="duration-150 ease-in hover:text-pink-600">
-                        <Link href="/projects">Projects</Link>
-                    </li>
-                    <li className="duration-150 ease-in hover:text-pink-600">
-                        <Link href="/contact">Reviews</Link>
-                    </li>
-                    <li className="duration-150 ease-in hover:text-pink-600">
-                        <Link href="/tests">Tests</Link>
-                    </li>
+                <ul className="flex gap-8 px-8 py-3 shadow-md dark:border-none border-[0.5px] dark:bg-zinc-700 rounded-3xl">
+                    <NavbarItems />
                 </ul>
-                <button className='px-4 py-3 text-2xl text-gray-500 duration-150 ease-in rounded-full bg-zinc-800 hover:text-cyan-400'>
-                    <IoSunnyOutline />
+                <button onClick={handleToggleTheme} className='px-4 py-3 text-2xl dark:text-inherit text-cyan-500 duration-150 ease-in rounded-full dark:bg-zinc-700 dark:hover:text-cyan-400 shadow-md dark:border-none border-[0.5px]'>
+                    {isDarkTheme ? <IoMoon /> :<IoSunnyOutline />}
                 </button>
             </nav>
         </header>
