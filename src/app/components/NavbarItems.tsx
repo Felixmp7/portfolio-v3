f083384c1819fa9df67c7fca8588d956eb9c2037
Felix Pacheco <devfex.19@gmail.com>
2023-08-28 15:18:44 -0400

    Add active styles to navbar items

--- src/app/components/NavbarItems.tsx
+++ src/app/components/NavbarItems.tsx
@@ -1,21 +1,54 @@
+'use client';
+
+import clsx from 'clsx';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const navigation = [
+    {
+        id: 1,
+        label: "About",
+        href: "/about",
+        color: 'text-pink-600'
+    },
+    {
+        id: 2,
+        label: "Projects",
+        href: "/projects",
+        color: 'text-blue-500'
+    },
+    {
+        id: 3,
+        label: "Reviews",
+        href: "/reviews",
+        color: 'text-yellow-600'
+    },
+    {
+        id: 4,
+        label: "Tests",
+        href: "/tests",
+        color: 'text-green-600'
+    }
+];
 
 
 export const NavbarItems = () => {
+    const pathname = usePathname();
+
     return (
         <>
-            <li className="duration-150 ease-in hover:text-pink-600">
-                <Link href="/about">About</Link>
-            </li>
-            <li className="duration-150 ease-in hover:text-blue-500">
-                <Link href="/projects">Projects</Link>
-            </li>
-            <li className="duration-150 ease-in hover:text-yellow-600">
-                <Link href="/contact">Reviews</Link>
-            </li>
-            <li className="duration-150 ease-in hover:text-green-600">
-                <Link href="/tests">Tests</Link>
-            </li>
+            {navigation.map(({ id, color, href, label }) => {
+                const classNames = clsx({
+                    'duration-150 ease-in': true,
+                    [`hover:${color}`]: true,
+                    [color]: pathname.includes(href)
+                });
+                return (
+                    <li key={id} className={classNames}>
+                        <Link href={href}>{label}</Link>
+                    </li>
+                );
+            })}
         </>
     );
 };
--- src/app/components/home/MainHeader.tsx
+++ src/app/components/home/MainHeader.tsx
@@ -17,7 +17,7 @@ export const MainHeader = () => (
             <h1 className='my-4 text-3xl font-semibold select-none md:my-8 md:text-5xl'>
                 Systems <span className='text-yellow-500 hover:underline'>engineer</span>,
                 <br /> <span className='text-cyan-400 hover:underline'>react</span> developer, and <br /> amateur soccer <span className='text-green-500 hover:underline'>player</span>.</h1>
-            <p className='max-w-md leading-5 md:max-w-xl md:leading-8 dark:text-zinc-400'>
+            <p className='max-w-md leading-5 md:max-w-xl md:leading-7 dark:text-zinc-400'>
                 Hello, I&apos;m Felix, a Systems Engineer from Caracas, Venezuela. I&apos;m 26 years old and I have been working as a React Developer since 2019.
             </p>
             <div className='mt-8'>
