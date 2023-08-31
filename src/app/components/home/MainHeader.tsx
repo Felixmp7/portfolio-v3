bbb89fbebf1ecd292c5171c12c9dad2392a1a312
Felix Pacheco <devfex.19@gmail.com>
2023-08-31 18:02:16 -0400

    Fix some responsive styles

--- src/app/components/home/MainHeader.tsx
+++ src/app/components/home/MainHeader.tsx
@@ -1,6 +1,7 @@
 import { SocialMedia } from '@/components/SocialMedia';
 import { Laptop3D } from '@/components/home/3DLaptop';
 import Image from 'next/image';
+import { getAge } from 'src/utils';
 
 
 export const MainHeader = () => (
@@ -18,7 +19,9 @@ export const MainHeader = () => (
                 Systems <span className='text-yellow-500 hover:underline'>engineer</span>,
                 <br /> <span className='text-cyan-400 hover:underline'>react</span> developer, and <br /> amateur soccer <span className='text-green-500 hover:underline'>player</span>.</h1>
             <p className='max-w-md leading-5 md:max-w-xl md:leading-7 dark:text-zinc-400'>
-                Hello, I&apos;m Felix, a Systems Engineer from Caracas, Venezuela. I&apos;m 26 years old and I have been working as a React Developer since 2019.
+                {`
+                    Hello, I'm Felix, a Systems Engineer from Caracas, Venezuela. I'm ${getAge()} years old and I have been working as a React Developer since 2019.
+                `}
             </p>
             <div className='mt-8'>
                 <SocialMedia />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             <section className='grid grid-cols-2 gap-10 mt-12 md:mt-24 md:grid-cols-4 h-80 xl:scale-105 md:h-60 lg:h-80'>
                 <MainRowImages />
             </section>
-            <div className='w-full h-[2px] my-8 md:my-20 bg-zinc-300 dark:bg-zinc-700' />
+            <div className='w-full h-[2px] my-8 md:mt-20 md:mb-10 bg-zinc-300 dark:bg-zinc-700' />
             <section>
                 <h2 className='text-3xl font-medium select-none md:text-5xl'>Main <strong className='font-extrabold hover:underline'>Stack</strong></h2>
                 <div className='mt-10 md:grid md:grid-cols-2 md:gap-16'>
