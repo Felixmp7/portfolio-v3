04c0417fa7d02e302ecf62b55df190a4d533cb76
Felix Pacheco <devfex.19@gmail.com>
2023-08-31 18:01:53 -0400

    Adjust styles of footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,17 @@
 import { NavbarItems } from '@/components/NavbarItems';
+import clsx from 'clsx';
 
-export const Footer = () => {
+type TProps = {
+    className?: string
+}
+
+export const Footer = ({ className = ''}: TProps) => {
+    const classNames = clsx({
+        'flex flex-col items-center space-y-4 dark:border-zinc-700 border-t-2 sm:py-5 sm:space-y-0 sm:flex-row sm:justify-between': true,
+        [className]: !!className
+    });
     return (
-        <footer className='flex flex-col items-center h-40 pt-10 space-y-4 sm:pt-20 sm:space-y-0 sm:flex-row sm:justify-between'>
+        <footer className={classNames}>
             <ul className="flex gap-8 py-3">
                 <NavbarItems />
             </ul>
