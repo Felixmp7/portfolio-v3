ddb413a47461b206581b798b1dfdc9c3a227fd63
Felix Pacheco <devfex.19@gmail.com>
2023-08-31 19:37:38 -0400

    Rename non-understandable variable name

--- src/utils.ts
+++ src/utils.ts
@@ -3,9 +3,9 @@ export const getAge = () => {
     const birthDate = new Date(1996, 7, 30);
 
     let age = today.getFullYear() - birthDate.getFullYear();
-    const m = today.getMonth() - birthDate.getMonth();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
 
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
         age--;
     }
 
