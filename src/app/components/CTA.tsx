92608509570a2a151e3dc568dc443ea0d0e3b3c6
Felix Pacheco <devfex.19@gmail.com>
2023-08-31 18:02:00 -0400

    Export new component CTA

---
+++ src/app/components/CTA.tsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+import Link from "next/link";
+
+type TProps = {
+    className?: string
+}
+
+export const CTA = ({ className = ''}:TProps) => {
+    const classNames = clsx({
+        'inline-block px-5 py-1.5 border transition-all duration-200 rounded-full dark:opacity-80 dark:bg-zinc-700 bg-zinc-100 hover:bg-zinc-200 dark:hover:opacity-100 border-zinc-300 dark:border-zinc-500': true,
+        [className]: !!className
+    });
+    return (
+        <Link href="/" className={classNames}>Hire</Link>
+    );
+};
