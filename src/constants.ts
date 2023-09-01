7a249436a5117c606873821bf6f319f4c0b88ee2
Felix Pacheco <devfex.19@gmail.com>
2023-09-01 17:32:58 -0400

    Add new home path on NAVIGATION config

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 import { TNavigationConfig } from "./types";
 
 export const NAVIGATION: TNavigationConfig = {
+    home: '/' as const,
     about: '/about' as const,
     projects: '/projects' as const,
     reviews: '/reviews' as const,
