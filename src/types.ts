5d5f97d20fb57ff1ec4cc6e4cf56f228de3312c5
Felix Pacheco <devfex.19@gmail.com>
2023-09-01 17:33:10 -0400

    Export new TTestResume type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,20 @@
 export type TNavigationConfig = {
+    home: string,
     about: string,
     projects: string,
     reviews: string,
     tests: string
+}
+
+export type TTestResume = {
+    id: number,
+    name: string;
+    position: string,
+    logo: string,
+    date: string,
+    context: string,
+    description: string,
+    deployLink: string,
+    repositoryLink: string,
+    wasHired: boolean
 }
