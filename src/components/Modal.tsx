c6f8592609176a4e147e55f57b95615e26e70aff
Felix Pacheco <devfex.19@gmail.com>
2023-09-05 13:01:38 -0400

    Add close button and hide body scroll when Modal is opened

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,6 +2,7 @@
 import { useClickOutside } from "@/hooks/useClickOutside";
 import { PropsWithChildren, useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
+import { SlClose } from 'react-icons/sl';
 
 type TProps = PropsWithChildren & {
     onClickOutside: VoidFunction
@@ -12,12 +13,23 @@ export default function Modal({ children, onClickOutside  }: TProps) {
     const modalRef = useRef<HTMLDivElement>(null);
 
     useClickOutside(modalRef, onClickOutside);
+
     useEffect(() => setMounted(true), []);
+    useEffect(() => {
+        document.body.style.overflow = 'hidden';
+        return () => {
+            document.body.style.overflow = 'unset';
+        };
+    }, []);
 
     return mounted ? createPortal(
         <div className="fixed inset-x-0 inset-y-0 z-10 grid text-sm bg-black backdrop-blur-sm bg-opacity-40 place-items-center">
-            <div ref={modalRef} className="dark:bg-zinc-600 bg-white dark:text-gray-300 text-zinc-700 w-[90%] sm:w-2/3 md:w-1/2 rounded-md p-6">
+            <div
+                ref={modalRef}
+                className="relative dark:bg-zinc-700 bg-white dark:text-gray-300 text-zinc-700 w-[80%] sm:w-2/3 md:w-1/2 rounded-md p-6"
+            >
                 {children}
+                <button className="absolute text-xl text-zinc-500 top-6 right-6" onClick={onClickOutside}><SlClose /></button>
             </div>
         </div>,
         document.body
