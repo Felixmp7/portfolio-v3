375536c2e567496ceba1f3910e93ee1bc5816eb5
Felix Pacheco <devfex.19@gmail.com>
2023-09-05 10:32:11 -0400

    Export new MainSection layout component

--- src/app/components/layout/MainSection.tsx
+++ src/app/components/layout/MainSection.tsx
@@ -2,6 +2,6 @@ import { PropsWithChildren } from "react";
 
 export const MainSection = ({ children }: PropsWithChildren) => {
     return (
-        <section className="pt-10 xl:pt-24">{children}</section>
+        <section className="pt-10 select-none xl:pt-24">{children}</section>
     );
 };
