828b47b3549cf553750ece5d3957474d0bd35c3a
Felix Pacheco <devfex.19@gmail.com>
2023-09-07 11:33:08 -0400

    Build function to download CV

--- src/components/home/latest-work/LatestWork.tsx
+++ src/components/home/latest-work/LatestWork.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { LatestWorkItem, TLatestWorkItem } from '@/components/home/latest-work/LatestWorkItem';
+import { saveAs } from 'file-saver';
 import { HiDocumentDownload } from 'react-icons/hi';
 import { MdOutlineWorkOutline } from 'react-icons/md';
 
@@ -27,6 +30,11 @@ const LATEST_JOBS: TLatestWorkItem[] = [
 ];
 
 export const LatestWork = () => {
+
+    const handleDownload = () => saveAs(
+        '/assets/files/cv-felix-pacheco-react-developer.pdf',
+        'cv-felix-pacheco-react-developer.pdf'
+    );
     return (
         <div className='w-full h-full p-5 border rounded-md border-zinc-300 dark:border-zinc-700'>
             <h3 className='flex items-center space-x-1'>
@@ -44,7 +52,10 @@ export const LatestWork = () => {
                     </li>
                 ))}
             </ul>
-            <button className='flex items-center justify-center w-full py-3 rounded-lg dark:opacity-80 dark:bg-zinc-700 bg-zinc-100 hover:bg-zinc-200 dark:hover:opacity-100'>
+            <button
+                className='flex items-center justify-center w-full py-3 rounded-lg dark:opacity-80 dark:bg-zinc-700 bg-zinc-100 hover:bg-zinc-200 dark:hover:opacity-100'
+                onClick={handleDownload}
+            >
                 Download CV <span className='ml-2 text-lg'>
                     <HiDocumentDownload />
                 </span>
