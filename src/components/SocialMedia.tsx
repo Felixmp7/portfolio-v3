c0ec271ec39a16da21a66ff043a5832fc363bcb9
Felix Pacheco <devfex.19@gmail.com>
2023-09-07 11:35:17 -0400

    Adjust social media layout

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -4,13 +4,13 @@ import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5';
 
 export const SocialMedia = () => {
     return (
-        <ul className='inline-flex items-center text-2xl gap-x-6'>
+        <ul className='inline-flex items-center text-2xl gap-x-3'>
             <li className='hover:text-blue-500'>
                 <Link href="https://www.linkedin.com/in/felix-pacheco-30/" target='_blank'>
                     <IoLogoLinkedin />
                 </Link>
             </li>
-            <li className='hover:text-white'>
+            <li className='hover:text-black dark:hover:text-white'>
                 <Link href="https://github.com/Felixmp7" target='_blank'>
                     <IoLogoGithub />
                 </Link>
