c051f5fefde210c91985613d02ec7b0bbf2066fb
Felix Pacheco <devfex.19@gmail.com>
2023-09-08 21:11:05 -0400

    Configure hostname of external assets

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'earnionc.sirv.com',
+                port: '',
+                pathname: '/portfolio-felix/**'
+            }
+        ]
+    }
+};
 
 module.exports = nextConfig;
