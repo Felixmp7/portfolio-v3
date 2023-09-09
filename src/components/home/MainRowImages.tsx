c43daa17fffd5b688c7384a6dc3bcb564d9d8f89
Felix Pacheco <devfex.19@gmail.com>
2023-09-08 21:16:44 -0400

    Import new BlurImage component

--- src/components/home/MainRowImages.tsx
+++ src/components/home/MainRowImages.tsx
@@ -1,37 +1,41 @@
-import Image from 'next/image';
+import BlurImage from '@/components/BlurImage';
 
 
 export const MainRowImages = () => {
     return (
         <>
             <div className='relative duration-150 ease-in-out hover:scale-105'>
-                <Image
+                <BlurImage
                     fill
-                    src="/assets/react.jpeg"
+                    quality={40}
+                    src="https://earnionc.sirv.com/portfolio-felix/react.jpeg"
                     alt="Felix Pacheco Avatar"
                     className='object-cover rounded-3xl md:rotate-6'
                 />
             </div>
             <div className='relative duration-150 ease-in-out hover:scale-105'>
-                <Image
+                <BlurImage
                     fill
-                    src="/assets/mountain.jpeg"
+                    quality={40}
+                    src="https://earnionc.sirv.com/portfolio-felix/mountain.jpeg"
                     alt="Felix Pacheco Avatar"
                     className='object-cover md:-translate-y-6 rounded-3xl md:rotate-1'
                 />
             </div>
             <div className='relative duration-150 ease-in-out hover:scale-105'>
-                <Image
+                <BlurImage
                     fill
-                    src="/assets/long-field.jpeg"
+                    quality={40}
+                    src="https://earnionc.sirv.com/portfolio-felix/long-field.jpeg"
                     alt="Felix Pacheco Avatar"
                     className='object-cover rounded-3xl md:-rotate-2'
                 />
             </div>
             <div className='relative duration-150 ease-in-out hover:scale-105'>
-                <Image
+                <BlurImage
                     fill
-                    src="/assets/team.jpeg"
+                    quality={40}
+                    src="https://earnionc.sirv.com/portfolio-felix/team.jpeg"
                     alt="Felix Pacheco Avatar"
                     className='object-cover md:translate-y-4 rounded-3xl md:rotate-3'
                 />
