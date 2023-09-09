99219f9e8b99a907f70dbf1588e14f2c09938e7a
Felix Pacheco <devfex.19@gmail.com>
2023-09-08 21:17:48 -0400

    Refactor LatestWorkItem

--- src/components/home/latest-work/LatestWorkItem.tsx
+++ src/components/home/latest-work/LatestWorkItem.tsx
@@ -1,24 +1,18 @@
 import { EnterpriseLogo } from '@/components/EnterpriseLogo';
+import { Slug, TProject } from 'src/types';
+import { getEnterpriseIcon } from 'src/utils';
 
-export type TLatestWorkItem = {
-    path: string;
-    alt: string;
-    enterpriseName: string;
-    job: string;
-    range: string
-}
-
-export const LatestWorkItem = ({ path, alt, enterpriseName, job, range }: TLatestWorkItem) => {
+export const LatestWorkItem = ({ enterpriseName, period, urlName }: TProject) => {
     return (
         <div className='flex items-end justify-between py-3'>
             <div className='flex items-center space-x-3'>
-                <EnterpriseLogo url={path} alt={alt} />
+                <EnterpriseLogo url={getEnterpriseIcon(enterpriseName.toLowerCase() as Slug)} alt={urlName} />
                 <span>
                     <h4>{enterpriseName}</h4>
-                    <span className='inline-block mt-1 font-light text-zinc-400'>{job}</span>
+                    <span className='inline-block mt-1 font-light text-zinc-400'>React Developer Ssr.</span>
                 </span>
             </div>
-            <span className='font-light text-zinc-400'>{range}</span>
+            <span className='font-light text-zinc-400'>{period}</span>
         </div>
     );
 };
