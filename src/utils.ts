25a5ff429f175aa6ed8893669d773cb4abc6164d
Felix Pacheco <devfex.19@gmail.com>
2023-09-08 21:12:08 -0400

    Export new util to getEnterpriseIcons

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { Slug } from "./types";
+
 export const getAge = () => {
     const today = new Date();
     const birthDate = new Date(1996, 7, 30);
@@ -10,4 +12,16 @@ export const getAge = () => {
     }
 
     return age;
+};
+
+export const getEnterpriseIcon = (slug: Slug) => {
+    const ICONS = {
+        'servicepad': '/assets/enterprises/servicepad.svg',
+        'matic': '/assets/enterprises/matic.svg',
+        'brocsoft': '/assets/enterprises/brocsoft.png',
+        'wadara': '/assets/enterprises/wadara.png',
+        'servisenior': '/assets/enterprises/servisenior.jpeg',
+        'reign': '/assets/enterprises/reign.jpeg'
+    };
+    return ICONS[slug];
 };
