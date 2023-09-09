f7009cb45ca2b8589362adb10206e365bc276c32
Felix Pacheco <devfex.19@gmail.com>
2023-09-08 21:15:30 -0400

    Update styles of Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -26,7 +26,7 @@ export default function Modal({ children, onClickOutside  }: TProps) {
         <div className="fixed inset-x-0 inset-y-0 z-10 grid text-sm bg-black backdrop-blur-sm bg-opacity-40 place-items-center">
             <div
                 ref={modalRef}
-                className="relative dark:bg-zinc-700 bg-white dark:text-gray-300 text-zinc-700 w-[80%] sm:w-2/3 md:w-1/2 rounded-md p-6"
+                className="relative dark:bg-zinc-700 bg-white dark:text-gray-300 text-zinc-700 w-[80%] sm:w-2/3 md:w-2/3 rounded-md p-6"
             >
                 {children}
                 <button className="absolute text-xl text-zinc-500 top-6 right-6" onClick={onClickOutside}><SlClose /></button>
