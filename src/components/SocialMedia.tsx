befd2d2c02cc0e7bc7db0c9a823a634a7626b760
Felix Pacheco <devfex.19@gmail.com>
2023-09-10 17:09:15 -0400

    Add aria-label attributes to social media links

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -6,12 +6,12 @@ export const SocialMedia = () => {
     return (
         <ul className='inline-flex items-center text-2xl gap-x-3'>
             <li className='hover:text-blue-500'>
-                <Link href="https://www.linkedin.com/in/felix-pacheco-30/" target='_blank'>
+                <Link aria-label='Linkedin profile link' href="https://www.linkedin.com/in/felix-pacheco-30/" target='_blank'>
                     <IoLogoLinkedin />
                 </Link>
             </li>
             <li className='hover:text-black dark:hover:text-white'>
-                <Link href="https://github.com/Felixmp7" target='_blank'>
+                <Link aria-label='Github repos link' href="https://github.com/Felixmp7" target='_blank'>
                     <IoLogoGithub />
                 </Link>
             </li>
