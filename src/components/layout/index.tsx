48ba0b320b414d7254c10618b2509cba90c869f0
Felix Pacheco <devfex.19@gmail.com>
2025-04-11 12:27:57 -0400

    feat: Remove deprecated code from layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,33 +1,16 @@
 'use client';
-import clsx from "clsx";
-import { usePathname } from "next/navigation";
 import { PropsWithChildren } from "react";
 
 import { Footer } from '@/components/Footer';
 import { Navbar } from '@/components/navigation/Navbar';
-import { NAVIGATION } from '@/constants';
 import { AppThemeProvider } from '@/providers/ThemeProvider';
 
-export const Layout = ({ children }:PropsWithChildren) => {
-    const pathname = usePathname();
-    const isTestsPath = pathname === NAVIGATION.tests;
-
-    const mainClasses = clsx({
-        'container container-bg': true,
-        'relative md:h-screen': isTestsPath
-    });
-
-    const footerClasses = clsx({
-        'mt-10': !isTestsPath,
-        'mt-10 md:mt-0 md:absolute px-10 bottom-0 inset-x-0': isTestsPath
-    });
-    return (
-        <AppThemeProvider>
-            <main className={mainClasses}>
-                <Navbar />
-                {children}
-                <Footer className={footerClasses} />
-            </main>
-        </AppThemeProvider>
-    );
-};
+export const Layout = ({ children }:PropsWithChildren) => (
+    <AppThemeProvider>
+        <main className="container container-bg">
+            <Navbar />
+            {children}
+            <Footer className="mt-10" />
+        </main>
+    </AppThemeProvider>
+);
