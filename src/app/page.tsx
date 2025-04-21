7d9c992f912ca6724508bcf6f0aa78c0f99376a4
Felix Pacheco <devfex.19@gmail.com>
2025-04-21 14:44:34 -0400

    refactor: Remove async fetching of experiences in Home component for simplified structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,9 @@ import { LatestWork } from '@/components/home/latest-work/LatestWork';
 import { MainHeader } from '@/components/home/MainHeader';
 import { MainRowImages } from '@/components/home/MainRowImages';
 import { MainStack } from '@/components/home/MainStack';
-import { getExperiences } from 'src/lib/services';
 
-const LATEST_EXPERIENCES_LIMIT = 5;
 
-export default async function Home() {
-    const latestExperiences = await getExperiences(LATEST_EXPERIENCES_LIMIT);
-
+export default function Home() {
     return (
         <section>
             <MainHeader />
@@ -21,7 +17,7 @@ export default async function Home() {
                 <h2 className='text-3xl font-medium select-none md:text-5xl'>Main <strong className='font-extrabold hover:underline'>Stack</strong></h2>
                 <div className='mt-0 sm:mt-10 md:grid md:grid-cols-2 md:gap-16'>
                     <MainStack />
-                    <LatestWork experiences={latestExperiences} />
+                    <LatestWork />
                 </div>
             </section>
         </section>
